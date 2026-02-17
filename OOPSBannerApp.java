b89a4e8198f8b4c067c2dae8317415b715bf297a
Lakkakula Charan teja <charant1807@gmail.com>
2026-02-17 09:17:17 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp - UC1
+ * 
+ * This program prints the literal text "OOPS" to the console.
+ * It demonstrates the basic structure of a Java application.
+ * 
+ * @author LAKKAKULA CHARAN TEJA
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
